9ce4248c850f984e53a2b07605555f288d774ac2
Connor Brown <cabrown802@gmail.com>
2025-06-29 13:45:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,9 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer as SIA
-import praw
-import matplotlib.pyplot as plt
-import math
+import asyncpraw
+import asyncio
 import datetime as dt
 import pandas as pd
-import numpy as np
-
-
-# In[2]:
-
 
 nltk.download('vader_lexicon')
 nltk.download('stopwords')
@@ -28,137 +16,100 @@ reddit = praw.Reddit(client_id='*********',
                     client_secret='******************',
                     user_agent='*********') ## to use this, make a Reddit app. Client ID is in top left corner, client secret is given, and user agent is the username that the app is under
 
-
-# In[4]:
-
-
 sub_reddits = reddit.subreddit('wallstreetbets')
-stocks = ["GME", "AMC"] 
+stocks = ["AMZN", "GOOG", "MSFT", "CRWD"]
 # For example purposes. To use this as a live trading tool, you'd want to populate this with tickers that have been mentioned on the pertinent community (WSB in our case) in a specified period.
-
-
-# In[5]:
-
-
-def commentSentiment(ticker, urlT):
-    subComments = []
-    bodyComment = []
-    try:
-        check = reddit.submission(url=urlT)
-        subComments = check.comments
-    except:
-        return 0
-    
-    for comment in subComments:
-        try: 
-            bodyComment.append(comment.body)
-        except:
-            return 0
-    
-    sia = SIA()
-    results = []
-    for line in bodyComment:
-        scores = sia.polarity_scores(line)
-        scores['headline'] = line
-
-        results.append(scores)
-    
-    df =pd.DataFrame.from_records(results)
-    df.head()
-    df['label'] = 0
-    
-    try:
-        df.loc[df['compound'] > 0.1, 'label'] = 1
-        df.loc[df['compound'] < -0.1, 'label'] = -1
-    except:
-        return 0
-    
-    averageScore = 0
-    position = 0
-    while position < len(df.label)-1:
-        averageScore = averageScore + df.label[position]
-        position += 1
-    averageScore = averageScore/len(df.label) 
-    
-    return(averageScore)
-
-
-# In[6]:
-
-
-def latestComment(ticker, urlT):
-    subComments = []
-    updateDates = []
-    try:
-        check = reddit.submission(url=urlT)
-        subComments = check.comments
-    except:
-        return 0
-    
-    for comment in subComments:
-        try: 
-            updateDates.append(comment.created_utc)
-        except:
-            return 0
-    
-    updateDates.sort()
-    return(updateDates[-1])
-
-
-# In[7]:
-
 
 def get_date(date):
     return dt.datetime.fromtimestamp(date)
 
-
-# In[8]:
-
-
-submission_statistics = []
-d = {}
-for ticker in stocks:
-    for submission in reddit.subreddit('wallstreetbets').search(ticker, limit=130):
-        if submission.domain != "self.wallstreetbets":
-            continue
-        d = {}
-        d['ticker'] = ticker
-        d['num_comments'] = submission.num_comments
-        d['comment_sentiment_average'] = commentSentiment(ticker, submission.url)
-        if d['comment_sentiment_average'] == 0.000000:
-            continue
-        d['latest_comment_date'] = latestComment(ticker, submission.url)
-        d['score'] = submission.score
-        d['upvote_ratio'] = submission.upvote_ratio
-        d['date'] = submission.created_utc
-        d['domain'] = submission.domain
-        d['num_crossposts'] = submission.num_crossposts
-        d['author'] = submission.author
-        submission_statistics.append(d)
+async def commentSentiment(submission):
+    await submission.load()
+    await submission.comments.replace_more(limit=0)
+    comments = submission.comments.list()
     
-dfSentimentStocks = pd.DataFrame(submission_statistics)
+    bodyComment = [comment.body for comment in comments if hasattr(comment, 'body')]
+    sia = SIA()
+    
+    results = []
+    for line in bodyComment:
+        scores = sia.polarity_scores(line)
+        scores['headline'] = line
+        results.append(scores)
 
-_timestampcreated = dfSentimentStocks["date"].apply(get_date)
-dfSentimentStocks = dfSentimentStocks.assign(timestamp = _timestampcreated)
+    if not results:
+        return 0
 
-_timestampcomment = dfSentimentStocks["latest_comment_date"].apply(get_date)
-dfSentimentStocks = dfSentimentStocks.assign(commentdate = _timestampcomment)
+    df = pd.DataFrame.from_records(results)
+    df['label'] = 0
+    df.loc[df['compound'] > 0.1, 'label'] = 1
+    df.loc[df['compound'] < -0.1, 'label'] = -1
+    
+    averageScore = df['label'].mean()
+    return averageScore
 
-dfSentimentStocks.sort_values("latest_comment_date", axis = 0, ascending = True,inplace = True, na_position ='last') 
+async def latestComment(submission):
+    await submission.load()
+    await submission.comments.replace_more(limit=0)
+    comments = submission.comments.list()
+    
+    if not comments:
+        return 0
 
-dfSentimentStocks
+    dates = [comment.created_utc for comment in comments if hasattr(comment, 'created_utc')]
+    return max(dates) if dates else 0
 
+async def main():
+    submission_statistics = []
 
-# In[9]:
+    for ticker in stocks:
+        subreddit = await reddit.subreddit("wallstreetbets")
+        print(f"Searching for {ticker}...")
+        count = 0
+        async for submission in subreddit.search(ticker, limit=200):
+            count += 1
+            await submission.load()
+            if submission.domain != "self.wallstreetbets":
+                continue
+            
+            print(f"Found submission {count} for {ticker}...")
+            
+            sentiment = await commentSentiment(submission)
+            if sentiment == 0:
+                continue
+            print(f"Loaded comments of submission {count} for {ticker}...")
+            latest_comment = await latestComment(submission)
 
+            d = {
+                'ticker': ticker,
+                'num_comments': submission.num_comments,
+                'comment_sentiment_average': sentiment,
+                'latest_comment_date': latest_comment,
+                'score': submission.score,
+                'upvote_ratio': submission.upvote_ratio,
+                'date': submission.created_utc,
+                'domain': submission.domain,
+                'num_crossposts': submission.num_crossposts,
+                'author': str(submission.author)
+            }
 
-dfSentimentStocks.author.value_counts()
+            submission_statistics.append(d)
+        print(f"Found {count} submissions for {ticker}")
 
+    df = pd.DataFrame(submission_statistics)
 
-# In[10]:
+    df["timestamp"] = df["date"].apply(get_date)
+    df["commentdate"] = df["latest_comment_date"].apply(get_date)
+    df.sort_values("latest_comment_date", inplace=True)
 
+    df.to_csv('Reddit_Sentiment_Equity2.csv', index=False) 
+    
+    print(df.head())
 
-dfSentimentStocks.to_csv('Reddit_Sentiment_Equity.csv', index=False) 
+loop = asyncio.get_event_loop()
 
-
-# In[ ]:
+if loop.is_running():
+    # For environments like Spyder or Jupyter
+    task = loop.create_task(main())
+else:
+    loop.run_until_complete(main())
